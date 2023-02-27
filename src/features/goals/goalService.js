1623533ceae75ee2085ffe6e989191c84b2974ba
Nikita Goncharenko <nikita.122495@gmail.com>
2023-02-27 22:00:44 +0100

    changed back API_URL

--- src/features/goals/goalService.js
+++ src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://backend-of-tasks-app.onrender.com/api/goals/";
+const API_URL = "/api/goals/";
 
 // Create new goal
 const createGoal = async (goalData, token) => {
