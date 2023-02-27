c7c281f6196d04810199b570cbdd5c45f126af85
Nikita Goncharenko <nikita.122495@gmail.com>
2023-02-27 21:20:53 +0100

    changed proxy back

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,5 @@
+export const host = "https://backend-of-tasks-app.onrender.com";
+export const loginRoute = `${host}/login`;
+export const registerRoute = `${host}/register`;
+export const logoutRoute = `${host}/logout`;
+export const allUsersRoute = `${host}/api/users/`;
